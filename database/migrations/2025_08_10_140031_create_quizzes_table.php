df73df7e6d5cbb14f0af888e60ffbf4bc4564aa6
Tajetti <talhettialvaro@edu.unifil.br>
2025-08-10 11:38:28 -0300

    Migrations e configuração inicial do banco SQLite

---
+++ database/migrations/2025_08_10_140031_create_quizzes_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('quizzes', function (Blueprint $table) {
+            $table->id();
+            $table->string('question');
+            $table->string('option');
+            $table->string("correct_answer");
+            $table->string('topic')->nullable();
+            $table->integer('score')->default(0);
+            $table->foreignId('courses_id')->constrained('courses')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('quizzes');
+    }
+};
