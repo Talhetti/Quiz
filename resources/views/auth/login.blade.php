f2b58656423c3ea8a091fe486e50846274b00bdb
Tajetti <talhettialvaro@edu.unifil.br>
2025-08-11 20:14:36 -0300

    Alterando tela de login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,10 +1,11 @@
 <x-layouts.auth :title="__('Login')">
     <!-- Login Card -->
     <div
-        class="bg-white dark:bg-gray-800 rounded-lg shadow-md border border-gray-200 dark:border-gray-700 overflow-hidden">
+        class="bg-white dark:bg-gray-800 rounded-lg shadow-md border border-gray-200 dark:border-gray-700 overflow-hidden">       
         <div class="p-6">
             <div class="mb-3">
-                <h1 class="text-2xl font-bold text-gray-800 dark:text-gray-100">{{ __('Log in to your account') }}</h1>
+                <h1 class="text-2xl font-bold text-gray-800 dark:text-gray-100 text-center">{{ __('Bem-vindo ao CodeQuiz') }}</h1>
+                <p class="text-sm text-center mt-2">{{ __('Aprenda programação com quizzes interativos')}} </p>
             </div>
 
             <form method="POST" action="{{ route('login') }}" class="space-y-3">
